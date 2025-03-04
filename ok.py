aafbb414cff0a3b6678f4feef5e75ebf8e885221
AJEGUM <juliandguevara12@gmail.com>
2025-03-03 23:07:59 -0500

    Entorno Py

---
+++ ok.py
@@ -0,0 +1,10 @@
+print("Hola Mundo")
+#   Para crear un entorno virtual deberas hacer lo siguiente:
+#   1- Instalar por consola el paquete "Virtualenv" para poder crear entornos virtuales con python, con el siguiente comando:
+#       pip install virtualenv
+#   2- Crear una carpeta y arrastrarla a tu IDE de preferencia
+#   3- Para crear el entorno virtual deberas de colocar el siguiente comando:
+#       python -m venv Nombre_del_Projecto - ejemplo: python -m venv test
+#   4- Si estas en Visual Studio Code podras observar que se te crearon varias carpetas las cuales estan relacionadas con el
+#       entorno virtual -- felicitaciones --
+#
